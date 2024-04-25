6a097eef56eaca1a23bd5a5e5ef104d9841fe313
damirpavlik <damir.pavlik@gmail.com>
2024-04-26 00:31:14 +0200

    feat: added memory pool

---
+++ shooter/src/game/pool.ts
@@ -0,0 +1,17 @@
+export class Pool<T> {
+    private pool: T[];
+    constructor(private ctor: () => T) {
+        this.pool = [];   
+    }
+
+    public get(): T | undefined {
+        if (this.pool.length > 0) {
+            return this.pool.pop();
+        }
+        return this.ctor();
+    }
+
+    public set(t: T) {
+        this.pool.push(t);
+    }
+}
